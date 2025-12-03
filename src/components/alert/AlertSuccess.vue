a5aa7e31d248996bdc171ce9cea0f572be5a7972
tairs <tairsafiulin@gmail.com>
2025-12-03 13:57:03 +0200

    INITIAL COMMIT

---
+++ src/components/alert/AlertSuccess.vue
@@ -0,0 +1,16 @@
+<template>
+  <div v-if="alertMessage !== ''" class="alert alert-success alert-dismissible fade show" role="alert">
+    {{ alertMessage }}
+    <button @click="$emit('event-alert-box-closed')" type="button" class="btn-close" data-bs-dismiss="alert"
+            aria-label="Close"></button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AlertSuccess',
+  props: {
+    alertMessage: String
+  }
+}
+</script>
---
+++ src/components/modal/Modal.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <!-- Modal -->
+    <div v-if="modalIsOpen" class="modal-overlay">
+      <div class="modal-container">
+        <div class="modal-content">
+          <div class="modal-header mb-2">
+            <h1 class="modal-title fs-5">
+              <slot name="title"></slot>
+            </h1>
+            <button type="button" class="btn-close" @click="$emit('event-close-modal')" aria-label="Close"/>
+          </div>
+          <div class="modal-body">
+            <slot name="body"></slot>
+          </div>
+          <div class="modal-footer mt-3 justify-content-center">
+            <slot name="buttons"></slot>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Modal',
+  props: {
+    modalIsOpen: Boolean
+  }
+};
+</script>
